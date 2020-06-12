09b089f99b80ec2feb30191753354de27226697f
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-06-12 22:13:17 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,23 @@
-class CreateStudents < ActiveRecord::Migration[5.1]
+class CreateStudents < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :students do |t|
+      t.string :name
+    end
+  end
+
 end
+
+
+# describe 'student' do
+#   before do
+#     sql = "DROP TABLE IF EXISTS students"
+#     ActiveRecord::Base.connection.execute(sql)
+#     CreateStudents.new.change
+#   end
+#
+#   it 'has a name' do
+#     student = Student.create(name: "Steven")
+#     expect(Student.where(name: "Steven").first).to eq(student)
+#   end
+# end
